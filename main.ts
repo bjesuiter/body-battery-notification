65eb0917f0407dd3700deba4491ef227b96dae82
Benjamin Jesuiter <bjesuiter@gmail.com>
2025-06-19 16:54:00 +0200

    enable cron and getDailySummary cron to see the db contents in dash.deno.com

--- main.ts
+++ main.ts
@@ -1,13 +1,21 @@
 import { router } from "./src/api/router.ts";
 import { getDailySummaryCron } from "./src/cron.ts";
 import { env } from "./src/env.ts";
-import { getDailySummary } from "./src/garmin-api.ts";
+import { getDailySummary, refreshTokens } from "./src/garmin-api.ts";
 
-// Deno.cron(
-//   "get_garmin_daily_summary_per_hour",
-//   { hour: { every: 1 } },
-//   getDailySummaryCron,
-// );
+Deno.cron(
+  "get_garmin_daily_summary_per_hour",
+  { hour: { every: 1 } },
+  getDailySummaryCron,
+);
+
+refreshTokens({
+  jwtFgp: env.JWT_FGP,
+  refreshToken: env.REFRESH_TOKEN,
+});
+
+// run once immediately on deployment
+getDailySummaryCron();
 
 // CAUTION: authenticate routes properly!
 router.listen({ port: 8080 });
--- src/api/router.ts
+++ src/api/router.ts
@@ -1,4 +1,6 @@
-import { Router } from "@oak/acorn";
+import { Router, v } from "@oak/acorn";
+import { storeAuth } from "../db.ts";
+import { refreshTokens } from "../garmin-api.ts";
 
 /**
  * oak/acorn docs: https://jsr.io/@oak/acorn
@@ -6,3 +8,22 @@ import { Router } from "@oak/acorn";
 export const router = new Router();
 
 router.get("/", () => ({ hello: "world" }));
+
+router.get("/set-auth", async (ctx) => {
+  const { jwtFgp, refreshToken } = await ctx.queryParams();
+  if (!jwtFgp || !refreshToken) {
+    ctx.throw(400, "jwtFgp and refreshToken are required");
+    return;
+  }
+  try {
+    const newAuth = await refreshTokens({ jwtFgp, refreshToken });
+    return {
+      message:
+        "Auth (jwtFgp, refreshToken and accessToken) refreshed successfully",
+    };
+  } catch (error) {
+    ctx.throw(500, "Failed to refresh tokens");
+    console.error(`Failed to refresh tokens`, error);
+    return;
+  }
+});
