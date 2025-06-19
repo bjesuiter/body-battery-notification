b2cabae59097477bf61ee8d4ca1ca8ca99c7185a
Benjamin Jesuiter <bjesuiter@gmail.com>
2025-06-19 14:42:37 +0200

    add retry to getDailySummary api fn

--- src/garmin-api.ts
+++ src/garmin-api.ts
@@ -1,4 +1,4 @@
-import ky from "ky";
+import ky, { HTTPError } from "ky";
 import { getOrInitAuth, storeAuth } from "./db.ts";
 import { Auth } from "./types/auth.type.ts";
 
@@ -42,15 +42,39 @@ export async function getDailySummary(userGuid: string) {
 
     // Example URL 
     // https://connect.garmin.com/usersummary-service/usersummary/daily/653930b5-079a-4685-a95f-5a9794a90269?calendarDate=2025-06-19
-    const response = await ky.get(`https://connect.garmin.com/usersummary-service/usersummary/daily/${userGuid}?calendarDate=${new Date().toISOString().split("T")[0]}`, {
-        headers: {
-            "Cookie": `JWT_FGP=${auth.jwtFgp}`,
-            "DI-Backend": "connectapi.garmin.com",
-            "Authorization": `Bearer ${auth.accessToken}`,
+        const response = await ky.get(`https://connect.garmin.com/usersummary-service/usersummary/daily/${userGuid}?calendarDate=${new Date().toISOString().split("T")[0]}`, {
+            headers: {
+                "Cookie": `JWT_FGP=${auth.jwtFgp}`,
+                "DI-Backend": "connectapi.garmin.com",
+                "Authorization": `Bearer ${auth.accessToken}`,
+            }, 
+            retry: {
+                limit: 3,
+                methods: ["GET"],
+                backoffLimit: 10000,
+            },
+            hooks: {
+                beforeRetry: [
+                    async ({request, error}: {request: Request, error: Error}) => {
+                        if (error instanceof HTTPError) {
+                            if (error.response.status === 403) {
+                                console.info("getDailySummary: failed with 403-NotAuthorized, refreshing tokens");
+                                const newAuth = await refreshTokens(auth);
+                                request.headers.set("Cookie", `JWT_FGP=${newAuth.jwtFgp}`);
+                                request.headers.set("Authorization", `Bearer ${newAuth.accessToken}`);
+                            }
+                        }
+
+                        console.error(`Unknown HTTP ERROR for getDailySummary:`, error);
+                    }
+                ]
+            }
+        });
+
+        if (!response.ok) {
+            console.error(`getDailySummary: failed with ${response.status} ${response.statusText}`, response.body);
+            // TODO: Send telegram message
         }
-    });
 
-    const data = await response.json();
-
-    return data;
+    return await response.json();
 }
