a4c88462f114e231ba1ae624a587a995f89f33a5
Benjamin Jesuiter <bjesuiter@gmail.com>
2025-06-19 11:49:52 +0200

    add .env reading

--- main.ts
+++ main.ts
@@ -1,8 +1,3 @@
-export function add(a: number, b: number): number {
-  return a + b;
-}
+const test = Deno.env.get("TEST");
 
-// Learn more at https://docs.deno.com/runtime/manual/examples/module_metadata#concepts
-if (import.meta.main) {
-  console.log("Add 2 + 3 =", add(2, 3));
-}
+console.log(test);
