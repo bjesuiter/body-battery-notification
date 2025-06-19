29941ce5affa9ecf1c09d2f20abe69fa818c0656
Benjamin Jesuiter <bjesuiter@gmail.com>
2025-06-19 11:53:21 +0200

    add zod parsing to env schema

--- src/env.ts
+++ src/env.ts
@@ -1,3 +1,12 @@
-export const env = {
+import { z } from "zod";
+
+const envSchema = z.object({
+    TEST: z.string(),
+})
+
+const envRaw = {
     TEST: Deno.env.get("TEST"),
 }
+
+export const env = envSchema.parse(envRaw);
+export type Env = z.infer<typeof envSchema>;
