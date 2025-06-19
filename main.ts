d604657cabfc37711653e278bc2dd3914850af6e
Benjamin Jesuiter <bjesuiter@gmail.com>
2025-06-19 14:46:42 +0200

    configure chronjob

--- main.ts
+++ main.ts
@@ -1,17 +1,8 @@
-import { getOrInitAuth } from "./src/db.ts";
+import { getDailySummaryCron } from "./src/cron.ts";
 import { env } from "./src/env.ts";
-import { getDailySummary, refreshTokens } from "./src/garmin-api.ts";
+import { getDailySummary } from "./src/garmin-api.ts";
 
-Deno.cron("Log a message", { hour: { every: 1 } }, () => {
-    console.log("This will print once an hour.");
-  });
-
-// const auth = await getOrInitAuth();
-
-// const newAuth = await refreshTokens({
-//     jwtFgp: auth.jwtFgp,
-//     refreshToken: auth.refreshToken,
-// });
+Deno.cron("get_garmin_daily_summary_per_hour", { hour: { every: 1 } }, getDailySummaryCron);
 
 const result = await getDailySummary(env.USER_GUID).catch((error) => {
     console.error(error);
---
+++ src/cron.ts
@@ -0,0 +1,11 @@
+import { env } from "./env.ts";
+import { getDailySummary } from "./garmin-api.ts";
+
+export async function getDailySummaryCron() {
+    const data = await getDailySummary(env.USER_GUID);
+    console.log(`getDailySummaryCron:`, data);
+
+    // TODO: store daily summary in db (once per hour)
+
+    // TODO: Send telegram message
+}
